0b037ed41764e5b17521f11492e356e171c2124e
Moong <moonm123@daum.net>
2015-04-15 14:36:06 +0900

    부릉부릉

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -4,27 +4,9 @@ public class CarTest {
 
 	public static void main(String[] args) {
 		
-		Car c1 = new Car();
-		Car c2 = new Car();
-		Car c3 = new Car();
-		
-		c1.setCompany("현대자동차");
-		c1.setModel("제네시스");
-		c1.setColor("검정색");
-		c1.setMaxSpeed(225);
-		c1.setPrice(50000000);
-		
-		c2.setCompany("기아자동차");
-		c2.setModel("K7");
-		c2.setColor("흰색");
-		c2.setMaxSpeed(245);
-		c2.setPrice(40000000);
-		
-		c3.setCompany("삼성자동차");
-		c3.setModel("SM7");
-		c3.setColor("회색");
-		c3.setMaxSpeed(200);
-		c3.setPrice(38000000);
+		Car2 c1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car2 c2 = new Car2("기아자동차", "K7", "흰색", 245, 40000000);
+		Car2 c3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
 		
 		String company = c1.getCompany();
 		String model = c1.getModel();
