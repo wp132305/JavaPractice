0e27bb45d06f55572ec59e6219332c9dd26f4744
kmseo <kmseo@DESKTOP-J4CLUPE>
2015-08-19 12:11:42 +0900

    도형나라 이야기

---
+++ JavaPractice/src/org/dimigo/inheritance/Circle.java
@@ -0,0 +1,28 @@
+package org.dimigo.inheritance;
+
+public class Circle extends Figure{
+	private int radius;
+	
+	public Circle(int radius) {
+		super(0, 0);
+		this.radius = radius;
+	}
+	
+	public Circle(int centerX, int centerY, int radius) {
+		super(centerX, centerY);
+		this.radius = radius;
+	}
+	
+	protected double calcArea() {
+		return radius*radius*Math.PI;
+	}
+
+	protected void printCenter() {
+		System.out.print("원 ");
+		super.printCenter();
+	}
+	
+	protected void moveFigure(int x, int y) {
+		super.moveFigure(x, y);
+	}
+}
