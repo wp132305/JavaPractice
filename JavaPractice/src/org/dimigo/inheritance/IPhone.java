ed51a6aed2215187e8d288a25175ec95e51695a3
wp132305 <anstjq98@naver.com>
2015-09-02 12:08:35 +0900

    da

--- JavaPractice/src/org/dimigo/inheritance/IPhone.java
+++ JavaPractice/src/org/dimigo/inheritance/IPhone.java
@@ -3,7 +3,7 @@ package org.dimigo.inheritance;
 public class IPhone extends SmartPhone{
 	public IPhone() {
 		
-	}
+	}		
 	
 	public IPhone(String model, String company, int price) {
 		super(model, company, price);
