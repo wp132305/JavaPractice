9333a45e281bb85c34c5ad9559419a8af00a8e67
wp132305 <anstjq98@naver.com>
2015-09-23 12:46:49 +0900

    Throw

---
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -0,0 +1,33 @@
+package org.dimigo.exception;
+
+public class MovieTest {
+	
+	public static void main(String[] args) {
+		
+			Movie[] movies = {
+					new Movie("베테랑", 15),
+					new Movie("앤트맨", 12), 
+					new Movie("사도", 12)
+			};
+			int age = 13;
+			
+			for(Movie movie : movies) {
+				try {
+					buyTicket(movie, age);
+				}
+				catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+	}
+	
+	private static void buyTicket(Movie movie, int age) throws AgeCheckException {
+		
+		if(age < movie.getLimitAge()) {
+			throw new AgeCheckException(movie);
+		}
+		
+		System.out.println(movie.getTitle() + "영화 즐감하세용~~");
+	}
+	
+}
